753e45ee6a72b2be63c372ff60c58e02c46f38f4
Raja Ramanathan <rajaramanathan@users.noreply.github.com>
2019-03-03 04:12:26 -0800

    Support for multiple games

--- test/TestLuckyDrawGame.sol
+++ test/TestLuckyDrawGame.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.9;
+pragma solidity >=0.4.0 <0.6.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
@@ -8,6 +8,8 @@ contract TestLuckyDrawGame {
 
     //need ether to start game
     uint public initialBalance = 10 ether;
+    address private gameId;
+    uint constant gameBounty = 5 ether;
 
     //use one instance for all the tests.
     LuckyDrawGame luckyDrawGame;
@@ -21,16 +23,34 @@ contract TestLuckyDrawGame {
     }
 
     function testStart() public {
-        luckyDrawGame.start.value(5 ether).gas(3000000)(10,6,1,30);
-        Assert.equal(5 ether, address(luckyDrawGame).balance,"Initial balance is incorrect");
+        gameId = luckyDrawGame.start.value(gameBounty).gas(3000000)(10,6,30);
+        Assert.equal(gameBounty, address(luckyDrawGame).balance,"Initial balance after start is incorrect");
+    }
+
+    function testGameCount() public {
+        Assert.equal(1, luckyDrawGame.getGameCount(), "There should be only one game");
+    }
+
+    function testGameIds() public {
+        Assert.equal(gameId, luckyDrawGame.getGameIds()[0], "GameId does not match");
+    }
+
+    function testGetGame() public {
+        (bool isOpen, uint balance, uint endTime) = luckyDrawGame.getGame(gameId);
+        Assert.equal(true, isOpen,"Initial balance after start is incorrect");
+        Assert.notEqual(0, balance, "balance was zero");
+        Assert.notEqual(0, endTime, "endTime was zero");
     }
 
     function testDraw() public payable {
-        luckyDrawGame.draw.gas(3000000)();
+        uint bounty = luckyDrawGame.draw.gas(3000000)(gameId,1);
+        Assert.notEqual(0, bounty, "draw was zero");
+        Assert.equal(gameBounty - bounty, address(luckyDrawGame).balance, "Balance after draw is incorrect.");
     }
 
     function testDrawMoreThanOnce() public {
-        (bool status,) = address(luckyDrawGame).call.gas(3000000)(abi.encodePacked("draw()"));
+        uint luckyNumber = 1;
+        (bool status,) = address(luckyDrawGame).call.gas(3000000)(abi.encodePacked("draw(address,uint)",gameId,luckyNumber));
         Assert.equal(false,status,"Call to second draw failed");
     }
 
